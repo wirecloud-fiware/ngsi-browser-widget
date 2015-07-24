a1e9245b10df3d520e372f9df27e11526a815849
Álvaro Arranz <aarranz@fi.upm.es>
2015-07-24 02:52:06 +0200

    Remove unused ngsi proxy preference

--- src/js/main.js
+++ src/js/main.js
@@ -31,7 +31,7 @@
 
         /* Preferences */
         MashupPlatform.prefs.registerCallback(function (newValues) {
-            if ('ngsi_server' in newValues || 'ngsi_proxy' in newValues || 'use_user_fiware_token' in newValues) {
+            if ('ngsi_server' in newValues || 'use_user_fiware_token' in newValues) {
                 this.updateNGSIConnection();
             }
             if ('extra_attributes' in newValues) {
@@ -59,10 +59,8 @@
     DataViewer.prototype.updateNGSIConnection = function updateNGSIConnection() {
 
         this.ngsi_server = MashupPlatform.prefs.get('ngsi_server');
-        this.ngsi_proxy = MashupPlatform.prefs.get('ngsi_proxy');
         this.ngsi_connection = new NGSI.Connection(this.ngsi_server, {
-            use_user_fiware_token: MashupPlatform.prefs.get('use_user_fiware_token'),
-            ngsi_proxy_url: this.ngsi_proxy
+            use_user_fiware_token: MashupPlatform.prefs.get('use_user_fiware_token')
         });
     };
 
